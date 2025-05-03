f9ca43738e38b2e6eb61835279a368fa23872d30
jaremsaunders <jarem.saunders@gmail.com>
2025-05-02 20:48:25 -0400

    updated file name schema

--- prompt_experiments.py
+++ prompt_experiments.py
@@ -55,39 +55,22 @@ api_key =  os.environ.get('GOOGLE_API_KEY')
 
 model_name = 'gemini-2.5-flash-preview-04-17'
 
+for domain in ["red_rising", "star_wars"]:
+    train, test, dev = get_train_test_dev_data(domain)
+    ontology = f'ontologies/{domain}.tsv'
+    entity_types = get_entity_types(ontology)
+
+    prompt = create_k_shot_prompt(train, 'star_wars', 'most_unique', 5, entity_types)
+    print(f"{prompt=}")
+
+    print(prompt)
+    reply = gemini_api_post_request(api_key, model_name, prompt)
+
+    print(reply)
+    print(reply['candidates'][0]['content']['parts'][0])
+    print(f"{reply=}")
 
 
-train, test, dev = get_train_test_dev_data('star_wars')
-ontology = 'ontologies/star_wars.tsv'
-entity_types = get_entity_types(ontology)
-
-prompt = create_k_shot_prompt(train, 'star_wars', 'most_unique', 5, entity_types)
-
-# stuck_sentences = preprocess_json('label_json/star_wars.json')
-# fixed_sentences = split_sentences(stuck_sentences)
-
-# sorted_sents = find_most_diverse(fixed_sentences,unique_only=True)
-
-
-rr_json = 'label_json/project-8-at-2025-04-25-10-07-1ab099cf.json'
-rr_ontology = 'ontologies/red_rising_ontology.tsv'
-
-stuck_sentences = preprocess_json(rr_json)
-fixed_sentences = split_sentences(stuck_sentences)
-
-sorted_sents = find_most_diverse(fixed_sentences,unique_only=True)
-ontology = 'ontologies/Star Wars Ontology.tsv'
-entity_types = get_entity_types(rr_ontology)
-prompt = task_definition_prompt(entity_types,examples=sorted_sents[:5])
-print(f"{prompt=}")
-
-
-print(prompt)
-reply = gemini_api_post_request(api_key, model_name, prompt)
-
-print(reply)
-print(reply['candidates'][0]['content']['parts'][0])
-print(f"{reply=}")
 
 
 
